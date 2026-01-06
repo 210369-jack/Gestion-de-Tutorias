e4eafc385ecb7f9856f3e95a5012255504d0c2a1
Jack Eliezer Ttito Huaman <210369@unsaac.edu.pe>
2026-01-05 19:58:27 -0500

    Actualizacion dashboard final

--- server.js
+++ server.js
@@ -80,9 +80,9 @@ const ActividadProfesional = mongoose.model("ActividadProfesional", ActividadPro
 // MODELO PARA GESTIÓN DE USUARIOS (HU1 y HU3 - Tu trabajo)
 // *** CAMBIO AQUÍ: Se agregó tutorActual ***
 const NuevoUsuarioSchema = new mongoose.Schema({
+  DNI: String,
   nombreCompleto: String,
-  email: String,
-  tipoUsuario: String,  
+  email: String, 
   matricula: String,
   tutorActual: { type: String, default: "Sin Asignar" }, // <--- NUEVO CAMPO PARA HU3
   fechaRegistro: { type: Date, default: Date.now } 
